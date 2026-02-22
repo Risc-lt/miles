6d51fc6692eceeebf84019a1f7acd4a677f5fff2
xinji1 <xinji1@microsoft.com>
2026-02-22 22:30:38 +0800

    misc

--- tests/test_weight_transfer_moe_multinode_kimik2_64nodes.py
+++ tests/test_weight_transfer_moe_multinode_kimik2_64nodes.py
@@ -192,7 +192,7 @@ def execute(args: ScriptArgs):
     sglang_args = (
         f"--rollout-num-gpus-per-engine {args.sglang_tp} "
         f"--rollout-num-gpus {args.num_rollout_gpus} "
-        "--sglang-mem-fraction-static 0.6 "
+        "--sglang-mem-fraction-static 0.8 "
         # "--sglang-enable-dp-attention "
         f"--sglang-dp-size {args.sglang_dp} "
         f"--sglang-ep-size {args.sglang_ep} "
