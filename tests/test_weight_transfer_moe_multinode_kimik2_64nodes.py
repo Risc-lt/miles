f7405867c5bb0fcfaa9b0e4e3340387f2c6d2e2d
xinji1 <xinji1@microsoft.com>
2026-03-02 01:19:03 +0800

    [temp] disable r-fork

--- tests/test_weight_transfer_moe_multinode_kimik2_64nodes.py
+++ tests/test_weight_transfer_moe_multinode_kimik2_64nodes.py
@@ -218,12 +218,12 @@ def execute(args: ScriptArgs, mode: str, base_log_dir: str):
         "--sglang-fp8-gemm-backend triton "
 
     )
-    if is_rdma:
-        sglang_args += "--sglang-remote-instance-weight-loader-start-seed-via-transfer-engine "
+    # if is_rdma:
+    #     sglang_args += "--sglang-remote-instance-weight-loader-start-seed-via-transfer-engine "
     if args.skip_validation:
         sglang_args += "--sglang-load-format dummy "
     else:
-        sglang_args +=         """--sglang-model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 8}' """
+        sglang_args += """--sglang-model-loader-extra-config '{"enable_multithread_load": true, "num_threads": 8}' """
     if args.sglang_dp > 1:
         sglang_args += "--sglang-enable-dp-attention "
     mem = int(args.bucket_size * 1024 * 1024 * 1024)
