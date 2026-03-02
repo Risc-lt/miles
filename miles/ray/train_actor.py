870e3992bbd575f50b8e36de97ac3caee6101df7
JD-ETH <jaedon.guo@gmail.com>
2026-03-02 01:27:47 +0000

    update

--- miles/ray/train_actor.py
+++ miles/ray/train_actor.py
@@ -67,6 +67,7 @@ class TrainRayActor(RayActor):
         dist.init_process_group(
             backend=backend,
             timeout=timedelta(minutes=args.distributed_timeout_minutes),
+            device_id=torch.device(f"cuda:{local_rank}"),
         )
         init_gloo_group()
 
