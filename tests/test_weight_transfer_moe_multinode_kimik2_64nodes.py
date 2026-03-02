00bd6d44ab74668d18d93e3d2ada60e4e298f2eb
xinji1 <xinji1@microsoft.com>
2026-03-02 12:12:14 +0800

    fix tokenizer issue

--- tests/test_weight_transfer_moe_multinode_kimik2_64nodes.py
+++ tests/test_weight_transfer_moe_multinode_kimik2_64nodes.py
@@ -4,7 +4,7 @@ from typing import Literal
 import typer
 import miles.utils.external_utils.command_utils as U
 from miles.utils.timer import log_experiment_start
-
+from transformers import AutoTokenizer
 MODEL_NAME = "Kimi-K2-Instruct"
 MODEL_TYPE = "kimi-k2"
 import time
@@ -71,6 +71,8 @@ def prepare(args: ScriptArgs):
         # U.exec_command(f"hf download moonshotai/Kimi-K2-Instruct --local-dir /root/models/{MODEL_NAME}")
         U.hf_download_dataset("zhuzilin/dapo-math-17k")
         U.hf_download_dataset("zhuzilin/aime-2024")
+    tokenizer = AutoTokenizer.from_pretrained(f"/root/models/{MODEL_NAME}/", trust_remote_code=True)
+    print("Tokenizer loaded, vocab size:", tokenizer.vocab_size)        
     # num_gpus = args.num_train_gpus + args.num_rollout_gpus
     # if not args.multinode:
     #     U.convert_checkpoint(model_name=MODEL_NAME, megatron_model_type=MODEL_TYPE, num_gpus_per_node=num_gpus)
