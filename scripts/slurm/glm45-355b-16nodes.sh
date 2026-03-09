995857e1dc66a123ea9d07d901bd3657019a0c68
JD-ETH <jaedon.guo@gmail.com>
2026-03-09 01:12:20 +0000

    new image

--- scripts/slurm/glm45-355b-16nodes.sh
+++ scripts/slurm/glm45-355b-16nodes.sh
@@ -20,7 +20,7 @@ EXEC_DATE=$(date +%Y-%m-%d_%H-%M-%S)
 EXP="${EXP:-glm355b-standalone}"
 
 # --------------- container / image config ---------------
-IMAGE_PATH="${IMAGE_PATH:-/mnt/vast/checkpoints/jiadongguo/docker_images/xinji1_miles.sqsh}"
+IMAGE_PATH="${IMAGE_PATH:-/mnt/vast/checkpoints/xinji1/images/docker_images/radixark_miles_dev-20260305.sqsh}"
 container_mounts="/mnt/vast/checkpoints/jiadongguo/rdma:/data"
 
 # --------------- NCCL / UCX env vars ---------------
